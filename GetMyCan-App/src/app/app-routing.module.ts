09bcc69cc3f0f5a7b7b446b2a481f58d7a82827a
sourabhdebnath <sourabh.debnath@decurtis.com>
2019-03-11 12:30:56 +0530

    Added google maps

--- GetMyCan-App/src/app/app-routing.module.ts
+++ GetMyCan-App/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'address', loadChildren: './pages/address/address.module#AddressPageModule' },
   { path: 'partner-login', loadChildren: './pages/partner-login/partner-login.module#PartnerLoginPageModule' },
   { path: 'partner-dashboard', loadChildren: './pages/partner-dashboard/partner-dashboard.module#PartnerDashboardPageModule' },
+  { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },
 ];
 
 @NgModule({
